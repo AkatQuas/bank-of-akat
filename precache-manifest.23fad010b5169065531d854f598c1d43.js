e9c89db5e2660caf66ecf4800ba90fb01068f400
AkatQuas <295140755@qq.com>
2020-02-16 18:45:59 +0800

    Updates

---
+++ precache-manifest.23fad010b5169065531d854f598c1d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff33969d9b75a8fd2ab32da3ec74026",
+    "url": "/bank-of-akat/index.html"
+  },
+  {
+    "revision": "6aec294e896f3afd7f7b",
+    "url": "/bank-of-akat/static/css/main.69be5e68.chunk.css"
+  },
+  {
+    "revision": "47d62ecd22cb98121752",
+    "url": "/bank-of-akat/static/js/2.13d7d641.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bank-of-akat/static/js/2.13d7d641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aec294e896f3afd7f7b",
+    "url": "/bank-of-akat/static/js/main.a29e386c.chunk.js"
+  },
+  {
+    "revision": "af509bd6d8a8c9c153c1",
+    "url": "/bank-of-akat/static/js/runtime-main.35c58e10.js"
+  }
+]);
